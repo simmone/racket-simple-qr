6e47f0e4661dc2ec0b194006320c541524982dab
Chen Xiao <chenxiao770117@gmail.com>
2015-06-01 21:34:07 +0800

    update

--- lab.rkt
+++ lab.rkt
@@ -2,22 +2,36 @@
 
 (require racket/draw)
 
-(define target (make-bitmap 100 100))
+(let* ([matrix 5]
+       [brick_width 10]
+       [canvas_width (* (+ matrix 2) brck_width)]
+       )
 
-(define dc (new bitmap-dc% [bitmap target]))
+  (define target (make-bitmap canvas_width canvas_width))
 
-(send dc set-pen "black" 4 'solid)
+  (define dc (new bitmap-dc% [bitmap target]))
+  (send dc set-smoothing 'smoothed)
+  (white-brush dc 0 0 canvas_width canvas_width)
 
-(send dc set-smoothing 'smoothed)
+  (send target save-file "box.png" 'png)
 
-(send dc draw-rectangle 0 0 100 100)
+  (system "open box.png")
+)
 
-(send dc set-pen "red" 1 'solid)
+(define (white-brush dc x y width height)
+  (send dc set-pen "white" 1 'solid)
+  (send dc set-brush "white" 'solid)
 
-(send dc draw-line 0 0 30 30)
+  (send dc draw-rectangle x y width height))
 
-(send dc draw-line 0 30 30 0)
+(define (black-brush dc x y width height)
+  (send dc set-pen "black" 1 'solid)
+  (send dc set-brush "black" 'solid)
 
-(send target save-file "box.png" 'png)
+  (send dc draw-rectangle x y width height))
 
-(system "open box.png")
+(define (locate-brick matrix row col)
+  )
+
+  
+  
