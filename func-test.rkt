30b21be7f262612bdd84d498bcc9285b850bdc5a
Chen Xiao <chenxiao770117@gmail.com>
2015-06-05 00:46:56 +0800

    update

--- func-test.rkt
+++ func-test.rkt
@@ -43,7 +43,19 @@
    (test-case
     "test-transform-points"
 
-    
+    (let* ([points_list
+            '((1 . 2) (3 . 4) (4 . 3))]
+           [transformed_points_list #f])
+      
+      (set! transformed_points_list
+            (transform-points-list points_list '(1 . 1)))
+      (check-equal? transformed_points_list '((1 . 2) (3 . 4) (4 . 3)))
+
+      (set! transformed_points_list
+            (transform-points-list points_list '(1 . 2)))
+      (check-equal? transformed_points_list '((1 . 3) (3 . 5) (4 . 4)))
+
+      ))
    
    ))
 
--- func.rkt
+++ func.rkt
@@ -2,6 +2,8 @@
 
 (require racket/draw)
 
+(require "define.rkt")
+
 (provide (contract-out
           [locate-brick (-> exact-nonnegative-integer?
                             pair?
@@ -23,21 +25,20 @@
                                    pair?))]
           [transform-points-list (-> list?
                                      pair?
-                                     exact-nonnegative-integer?
                                      list?)]
           ))
 
-(define (white-block dc place_pair size_pair)
+(define (white-block dc place_pair module_width)
   (send dc set-pen "white" 1 'solid)
   (send dc set-brush "white" 'solid)
 
-  (send dc draw-rectangle (car place_pair) (cdr place_pair) (car size_pair) (cdr size_pair)))
+  (send dc draw-rectangle (car place_pair) (cdr place_pair) module_width module_width))
 
-(define (black-block dc place_pair size_pair)
+(define (black-block dc place_pair module_width)
   (send dc set-pen "black" 1 'solid)
   (send dc set-brush "black" 'solid)
 
-  (send dc draw-rectangle (car place_pair) (cdr place_pair) (car size_pair) (cdr size_pair)))
+  (send dc draw-rectangle (car place_pair) (cdr place_pair) module_width module_width))
 
 (define (locate-brick module_width place_pair)
   (cons (+ module_width (* (sub1 (cdr place_pair)) module_width))
@@ -49,9 +50,26 @@
    (cons (- (* version module_width) module_width (* 7 module_width)) module_width)
    (cons module_width (- (* version module_width) module_width (* 7 module_width)))))
 
-(define (transform-points-list points_list place_pair module_width)
-  '())
+(define (transform-points-list points_list start_point_pair)
+  (map
+   (lambda (point)
+     (cons (+ (car start_point_pair) (sub1 (car point))) (+ (cdr start_point_pair) (sub1 (cdr point)))))
+    points_list))
 
-(define (draw-finder-patter dc version module_width)
+(define (draw-finder-pattern dc module_width)
+  (for-each
+   (lambda (point_pair)
+     (black-block dc (locate-brick module_width point_pair) module_width))
+   (first *finder_pattern_points*))
+
+  (for-each
+   (lambda (point_pair)
+     (white-block dc (locate-brick module_width point_pair) module_width))
+   (second *finder_pattern_points*))
+
+  (for-each
+   (lambda (point_pair)
+     (black-block dc (locate-brick module_width point_pair) module_width))
+   (third *finder_pattern_points*))
   )
 
