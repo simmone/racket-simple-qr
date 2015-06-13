ec23fc8e5e185baf057a7c5883f8b2a9192749ae
Chen Xiao <chenxiao770117@gmail.com>
2015-06-13 09:37:52 +0800

    update

--- simple-qr/lib/func/capacity/capacity-func-test.rkt
+++ simple-qr/lib/func/capacity/capacity-func-test.rkt
@@ -12,11 +12,13 @@
     "test-get-version"
 
     (check-equal? (get-version 10 "B" "Q") 1)
+    (check-equal? (get-version 1663 "B" "Q") 40)
     (check-equal? (get-version 30 "B" "L") 2)
-    (check-equal? (get-version 30 "B" "H") 3)
+    (check-equal? (get-version 30 "B" "H") 4)
     (check-equal? (get-version 100 "A" "L") 4)
     (check-equal? (get-version 2100 "B" "L") 34)
     (check-equal? (get-version 7089 "N" "L") 40)
+    (check-equal? (get-version 1664 "B" "Q") 40)
     )
 
    ))
--- simple-qr/lib/func/capacity/capacity-func.rkt
+++ simple-qr/lib/func/capacity/capacity-func.rkt
@@ -8,6 +8,13 @@
                 exact-nonnegative-integer?)]
   ))
 
+(require "capacity-dic.rkt")
 
 (define (get-version char_count mode error_level)
-  0)
+  (let loop ([loop_list (hash-ref *capacity_table* (string-append mode "-" error_level))])
+    (if (not (null? loop_list))
+        (if (<= char_count (cdar loop_list))
+            (caar loop_list)
+            (loop (cdr loop_list)))
+        (error (string-append "no such version: mode[" mode "]error_level[" error_level "]char_count[" (number->string char_count) "]")))))
+          
