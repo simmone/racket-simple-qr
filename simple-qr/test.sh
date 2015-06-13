cb7d2796870bbdd492dc8878e9913c0eb712b5e6
Chen Xiao <chenxiao770117@gmail.com>
2015-06-13 15:57:56 +0800

    update

--- simple-qr/test.sh
+++ simple-qr/test.sh
@@ -1,7 +1,8 @@
 HOME_DIR="racket-simple-qr/simple-qr"
 echo "func";cd; cd $HOME_DIR/lib/func;racket func-test.rkt
-echo "func/capacity_table";cd; cd $HOME_DIR/lib/func/capacity;racket capacity-dic-test.rkt
-echo "func/capacity_table";cd; cd $HOME_DIR/lib/func/capacity;racket capacity-func-test.rkt
+echo "func/capacity";cd; cd $HOME_DIR/lib/func/capacity;racket capacity-dic-test.rkt
+echo "func/capacity";cd; cd $HOME_DIR/lib/func/capacity;racket capacity-func-test.rkt
+echo "func/character-count";cd; cd $HOME_DIR/lib/func/character-count;racket character-count-test.rkt
 echo "timing-pattern";cd; cd $HOME_DIR/lib/timing-pattern;racket timing-pattern-test.rkt
 echo "alignment-pattern";cd; cd $HOME_DIR/lib/alignment-pattern;racket alignment-pattern-test.rkt
 
