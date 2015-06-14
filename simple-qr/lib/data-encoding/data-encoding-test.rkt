4e96e5c0a450318e6b588cc7ae0ca4aed683fb14
Chen Xiao <chenxiao770117@gmail.com>
2015-06-14 16:13:48 +0800

    update

--- simple-qr/lib/data-encoding/data-encoding-test.rkt
+++ simple-qr/lib/data-encoding/data-encoding-test.rkt
@@ -5,12 +5,12 @@
 (require rackunit "data-encoding.rkt")
 
 (define test-data-encoding
-  (test-suite 
+  (test-suite
    "test-data-encoding"
 
-   (test-case 
+   (test-case
     "test-locate-data-encoding-joints"
-    
+
     (let* ([url "http://chenxiao.info"]
            [mode "B"]
            [error_level "H"]
@@ -23,11 +23,35 @@
       (check-equal? character_count_indicator "00010100")))
 
    (test-case
-    "test-encoding-b"
-    
-    (check-equal? (encoding-b "Hello") "0100100001100101011011000110110001101111")
+    "test-encode-b"
+
+    (check-equal? (encode-b "Hello") "0100100001100101011011000110110001101111")
     )
-   
+
+   (test-case
+    "test-num-split-three"
+
+    (check-equal? (num-split-three "10000") '(100 0))
+    (check-equal? (num-split-three "10001") '(100 1))
+    (check-equal? (num-split-three "12345") '(123 45))
+    (check-equal? (num-split-three "1234567") '(123 456 7))
+    (check-equal? (num-split-three "123456") '(123 456))
+    (check-equal? (num-split-three "1") '(1))
+    (check-equal? (num-split-three "12") '(12))
+    (check-equal? (num-split-three "123") '(123))
+    (check-equal? (num-split-three "1234") '(123 4))
+    (check-equal? (num-split-three "01234") '(12 34))
+    (check-equal? (num-split-three "001234") '(1 234))
+    (check-equal? (num-split-three "0001234") '(0 123 4))
+    )
+
+   (test-case
+    "test-encode-n"
+    
+    (check-equal? (encode-n "1234567") "1111011111001000111")
+    )
+    
+
    ))
 
 (run-tests test-data-encoding)
--- simple-qr/lib/data-encoding/data-encoding.rkt
+++ simple-qr/lib/data-encoding/data-encoding.rkt
@@ -4,7 +4,9 @@
           [get-version (-> string? string? string? exact-nonnegative-integer?)]
           [get-mode-indicator (-> string? string?)]
           [get-character-count-indicator (-> exact-nonnegative-integer? exact-nonnegative-integer? string? string?)]
-          [encoding-b (-> string? string?)]
+          [encode-b (-> string? string?)]
+          [num-split-three (-> string? list?)]
+          [encode-n (-> string? string?)]
           ))
 
 (require "../func/global.rkt")
@@ -22,7 +24,7 @@
 (define (get-character-count-indicator character_count version mode)
   (~r character_count #:base 2 #:min-width (get-character-bit-width version mode) #:pad-string "0"))
 
-(define (encoding-b content)
+(define (encode-b content)
   (with-output-to-string
     (lambda ()
       (for-each
@@ -30,4 +32,18 @@
          (printf "~a" (~r char_byte #:base 2 #:min-width 8 #:pad-string "0")))
        (bytes->list (string->bytes/latin-1 content))))))
 
-  
+(define (num-split-three num_str)
+  (reverse
+   (let loop ([loop_str num_str]
+              [result_list '()])
+     (if (<= (string-length loop_str) 3)
+         (cons (string->number loop_str) result_list)
+         (loop (substring loop_str 3) (cons (string->number (substring loop_str 0 3)) result_list))))))
+
+(define (encode-n content)
+  (with-output-to-string
+    (lambda ()
+      (for-each
+       (lambda (num)
+         (printf "~a" (~r num #:base 2)))
+       (num-split-three content)))))
