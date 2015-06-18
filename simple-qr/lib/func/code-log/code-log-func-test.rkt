39096fbcf46a10742fef78a19598859815bc16dd
Chen Xiao <chenxiao770117@gmail.com>
2015-06-18 11:33:26 +0800

    update

---
+++ simple-qr/lib/func/code-log/code-log-func-test.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require rackunit/text-ui)
+
+(require rackunit "code-log-func.rkt")
+
+(define test-code-log-func
+  (test-suite 
+   "test-code-log-func"
+
+   (test-case
+    "test-a->value"
+
+    (check-equal? (a->value 40) 106)
+    )
+
+   (test-case
+    "test-value->a"
+
+    (check-equal? (value->a 106) 40)
+    )
+
+   ))
+
+(run-tests test-code-log-func)
