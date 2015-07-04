76eae3dfe38314c8911d237a35e6db8961d75d37
Chen Xiao <chenxiao770117@gmail.com>
2015-07-04 16:18:16 +0800

    update

--- simple-qr/lib/dark-module/dark-module.rkt
+++ simple-qr/lib/dark-module/dark-module.rkt
@@ -3,9 +3,9 @@
 (require "../func/func.rkt")
 
 (provide (contract-out
-          [draw-dark-module (-> exact-nonnegative-integer? hash? void?)]
+          [draw-dark-module (-> exact-nonnegative-integer? hash? hash? void?)]
           ))
 
-(define (draw-dark-module version points_map)
+(define (draw-dark-module version points_map type_map)
   (let ([point_pair  (cons (+ (* 4 version) 10) 9)])
-    (hash-set! points_map point_pair '("1" . "dark"))))
+    (add-point point_pair "1" "dark" points_map type_map)))
--- simple-qr/main.rkt
+++ simple-qr/main.rkt
@@ -38,7 +38,15 @@
       (draw-dark-module version points_map type_map)
       
       (trace (format "finder[~a]separator[~a]timing[~a]alignment[~a]format[~a]version[~a]\n"
-                     
+                     (length (filter (lambda (rec) (string=? (cdr rec) "finder")) (hash->list type_map)))
+                     (length (filter (lambda (rec) (string=? (cdr rec) "separator")) (hash->list type_map)))
+                     (length (filter (lambda (rec) (string=? (cdr rec) "timing")) (hash->list type_map)))
+                     (length (filter (lambda (rec) (string=? (cdr rec) "alignment")) (hash->list type_map)))
+                     (length (filter (lambda (rec) (string=? (cdr rec) "format")) (hash->list type_map)))
+                     (length (filter (lambda (rec) (string=? (cdr rec) "version")) (hash->list type_map))))
+             1)
+      
+      (trace (format "remain_points:~a\n" (- sum_count (hash-count points_map))) 1)
 
       (let ([data_list (string->list (matrix-data data #:version version #:mode mode #:error_level error_level))]
             [trace_list (snake-modules modules #:skip_points_hash points_map)])
@@ -46,9 +54,9 @@
         (draw-data data_list trace_list points_map))
 
       (let ([mask_number (mask-data modules points_map)])
-        (draw-format-information error_level mask_number modules points_map))
+        (draw-format-information error_level mask_number modules points_map type_map))
 
-      (draw-version-information version modules points_map)
+      (draw-version-information version modules points_map type_map)
 
       (let* ([canvas_width (* (+ modules 8) module_width)]
              [target (make-bitmap canvas_width canvas_width)]
