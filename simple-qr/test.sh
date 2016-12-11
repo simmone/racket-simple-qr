b3bcb97105c40f5baeeb2bc34436a6f3aa05eca0
simmone <chenxiao770117@gmail.com>
2016-12-11 22:11:19 +0800

    update

--- simple-qr/test.sh
+++ simple-qr/test.sh
@@ -1,18 +1,1 @@
-HOME_DIR="racket-simple-qr/simple-qr"
-echo "func";cd; cd $HOME_DIR/lib/func;racket func-test.rkt
-echo "func/capacity";cd; cd $HOME_DIR/lib/func/capacity;racket capacity-dic-test.rkt
-echo "func/capacity";cd; cd $HOME_DIR/lib/func/capacity;racket capacity-func-test.rkt
-echo "func/character-count";cd; cd $HOME_DIR/lib/func/character-count;racket character-bit-width-test.rkt
-echo "timing-pattern";cd; cd $HOME_DIR/lib/timing-pattern;racket timing-pattern-test.rkt
-echo "alignment-pattern";cd; cd $HOME_DIR/lib/alignment-pattern;racket alignment-pattern-test.rkt
-echo "data-encoding";cd; cd $HOME_DIR/lib/data-encoding;racket data-encoding-test.rkt
-echo "func/code-info/code-info-dic";cd; cd $HOME_DIR/lib/func/code-info;racket code-info-dic-test.rkt
-echo "func/code-info/code-info-func";cd; cd $HOME_DIR/lib/func/code-info;racket code-info-func-test.rkt
-echo "func/remainder-bits/remainder-bits-func";cd; cd $HOME_DIR/lib/func/remainder-bits;racket remainder-bits-func-test.rkt
-echo "func/poly";cd; cd $HOME_DIR/lib/func/poly;racket poly-dic-func-test.rkt
-echo "error-correct-code/poly-func";cd; cd $HOME_DIR/lib/error-correct-code;racket poly-func-test.rkt
-echo "error-correct-code";cd; cd $HOME_DIR/lib/error-correct-code;racket error-correct-code-test.rkt
-echo "fill-data";cd; cd $HOME_DIR/lib/fill-data;racket fill-data-test.rkt
-echo "mask-data";cd; cd $HOME_DIR/lib/mask-data;racket mask-data-test.rkt
-echo "lib";cd; cd $HOME_DIR/lib/;racket lib-test.rkt
-
+find . -name '*-test.rkt' -exec racket {} \; -print
