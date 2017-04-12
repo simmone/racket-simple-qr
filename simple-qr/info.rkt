f56c201cfb4f565cc216b596be1e769c7ebea75c
simmone <chenxiao770117@gmail.com>
2017-04-12 22:50:11 +0800

    update

--- simple-qr/info.rkt
+++ simple-qr/info.rkt
@@ -2,10 +2,7 @@
 (define scribblings
   '(("scribble/simple-qr.scrbl" (multi-page) (tool 100))))
 
-(define compile-omit-paths '("tests"))
-(define test-omit-paths '("tests"))
-
-(define compile-omit-paths '("standards"))
-(define test-omit-paths '("standards"))
+(define compile-omit-paths '("tests" "standards"))
+(define test-omit-paths '("tests" "standards"))
 
 
