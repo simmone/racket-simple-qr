709154c7e4156381943ff24134b23cbc31e07570
Chen Xiao <chenxiao770117@gmail.com>
2021-02-11 22:24:18 +0800

    update

--- simple-qr/info.rkt
+++ simple-qr/info.rkt
@@ -3,6 +3,6 @@
   '(("scribble/simple-qr.scrbl" (multi-page) (tool 100))))
 
 (define compile-omit-paths '("tests" "standards"))
-(define test-omit-paths '("tests" "standards"))
+(define test-include-paths '("tests"))
 
 
