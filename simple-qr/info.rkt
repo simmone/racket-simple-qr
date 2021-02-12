87b7b325104fa80c23e603a440a0336e4efe70ef
Chen Xiao <chenxiao770117@gmail.com>
2021-02-12 11:53:03 +0800

    update

--- simple-qr/info.rkt
+++ simple-qr/info.rkt
@@ -2,7 +2,7 @@
 (define scribblings
   '(("scribble/simple-qr.scrbl" (multi-page) (tool 100))))
 
-(define compile-omit-paths '("tests" "standards"))
-(define test-includes-paths '("tests"))
+(define compile-omit-paths '("tests" "standards" "example"))
+(define test-omit-paths '("info.rkt" "main.rkt" "standards" "example" "scribble" "share" "read" "write"))
 
 
