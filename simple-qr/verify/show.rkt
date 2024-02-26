f341169aa3e75c00531f19eaed0d7592fcccdc59
Chen Xiao <chenxiao770117@gmail.com>
2024-02-26 16:35:40 +0800

    update

--- simple-qr/verify/show.rkt
+++ simple-qr/verify/show.rkt
@@ -5,8 +5,4 @@
 (require racket/runtime-path)
 (define-runtime-path content_directory (build-path "express" "content"))
 
-;(delete-directory/files content_directory)
-
-;(make-directory content_directory)
-
-(qr-write-express "chenxiao" "chenxiao.png")
+(qr-write-express "chenxiao" "chenxiao.svg" #:output_type 'svg)
