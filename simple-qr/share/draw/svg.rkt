5528996f0bcf770cd89b774c8116e3c6ef842cdf
Chen Xiao <chenxiao770117@gmail.com>
2024-03-20 04:45:23 +0800

    update

--- simple-qr/share/draw/svg.rkt
+++ simple-qr/share/draw/svg.rkt
@@ -13,31 +13,34 @@
   (with-output-to-file
       file_name #:exists 'replace
       (lambda ()
-        (printf "~a"
-                (svg-out
-                 (QR-canvas_width qr) (QR-canvas_width qr)
-                 (lambda ()
-                   (let ([basic_brick (svg-def-shape (new-rect (QR-module_width qr) (QR-module_width qr)))]
-                         [color_style_map (make-hash)])
-                     
-                     (for-each
-                      (lambda (color)
-                        (hash-set! color_style_map color #f))
-                      (hash-values (QR-points_color_map qr)))
+        (printf
+         "~a"
+         (svg-out
+          (QR-canvas_width qr) (QR-canvas_width qr)
+          (lambda ()
+            (let ([basic_brick (svg-def-shape (new-rect (QR-module_width qr) (QR-module_width qr)))]
+                  [color_style_map (make-hash)])
+              
+              (for-each
+               (lambda (color)
+                 (hash-set! color_style_map color #f))
+               (hash-values (QR-points_color_map qr)))
 
-                     (for-each
-                      (lambda (color)
-                        (let ([sstyle (new-sstyle)])
-                          (set-SSTYLE-fill! _sstyle color)
-                          (hash-set! color_style_map color sstyle)))
-                      (hash-keys color_style_map))
+              (for-each
+               (lambda (color)
+                 (let ([sstyle (new-sstyle)])
+                   (set-SSTYLE-fill! _sstyle color)
+                   (hash-set! color_style_map color sstyle)))
+               (hash-keys color_style_map))
 
-                     (let loop ([points (QR-points qr)])
-                       (when (not (null? points))
-                         (when (= (cdar points_list) 1)
-                           (let ([new_point_pair (cons (+ (cdaar points_list) 4) (+ (caaar points_list) 4))])
-                             (svg-place-widget
-                              rect
-                              #:style rect_sstyle
-                              #:at (locate-brick module_width new_point_pair))))
-                         (loop (cdr points_list)))))))))))
+              (let loop ([points (QR-points qr)])
+                (when (not (null? points))
+                  (let* ([point (car points)]
+                         [color (hash-ref (QR-points_color_map qr) point )]
+                         [style (hash-ref color_style_map color)])
+
+                      (svg-place-widget
+                       basic_brick
+                       #:style style
+                       #:at (locate-brick module_width point)))
+                  (loop (cdr points)))))))))))
--- simple-qr/tests/share/draw/draw-test.rkt
+++ simple-qr/tests/share/draw/draw-test.rkt
@@ -8,6 +8,8 @@
          "../../../share/finder-pattern.rkt"
          racket/runtime-path)
 
+(define-runtime-path png_1X1_file "png_1X1.png")
+(define-runtime-path svg_1X1_file "svg_1X1.svg")
 (define-runtime-path canvas_png_file "canvas.png")
 (define-runtime-path canvas_svg_file "canvas.svg")
 (define-runtime-path fill_canvas_png_file "fill_canvas.png")
@@ -40,78 +42,94 @@
           ;(delete-file canvas_png_file)
           ;(delete-file canvas_svg_file)
           )))
-
-   (test-case
-    "test-fill-canvas"
-    
-    (dynamic-wind
-        (lambda () (void))
-        (lambda ()
-          (let ([qr (new-default-qr "chenxiao")])
-            (set-QR-module_width! qr 20)
-
-            (draw qr fill_canvas_png_file 'png)
-            (draw qr fill_canvas_svg_file 'svg)
-            ))
-        (lambda ()
-          ;(void)
-          (delete-file fill_canvas_png_file)
-          (delete-file fill_canvas_svg_file)
-          )))
-
-   (test-case
-    "test-pattern-canvas"
-    
-    (dynamic-wind
-        (lambda () (void))
-        (lambda ()
-          (let ([qr (new-default-qr "chenxiao")])
-            (set-QR-module_width! qr 20)
-
-            (draw qr pattern_canvas_png_file 'png)
-            (draw qr pattern_canvas_svg_file 'svg)
-            ))
-        (lambda ()
-          ;(void)
-          (delete-file pattern_canvas_png_file)
-          (delete-file pattern_canvas_svg_file)
-          )))
-   
-   (test-case
-    "test-transparent-canvas"
-
-    (dynamic-wind
-        (lambda () (void))
-        (lambda ()
-          (let ([qr (new-default-qr "chenxiao")])
-            (set-QR-module_width! qr 20)
-
-            (draw qr transparent_canvas_png_file 'png)
-            (draw qr transparent_canvas_svg_file 'svg)
-            ))
-        (lambda ()
-          ;(void)
-          (delete-file transparent_canvas_png_file)
-          (delete-file transparent_canvas_svg_file)
-          )))
-
-   (test-case
-    "test-fill-points"
-    
-    (dynamic-wind
-        (lambda () (void))
-        (lambda ()
-          (let ([qr (new-default-qr "chenxiao")])
-            (set-QR-module_width! qr 20)
-
-            (draw qr normal_canvas_png_file 'png)
-            (draw qr normal_canvas_svg_file 'svg)
-            ))
-        (lambda ()
-          ;(void)
-          (delete-file normal_canvas_png_file)
-          (delete-file normal_canvas_svg_file)
-          )))
+;
+;   (test-case
+;    "test-init-canvas-with-quiet-zone"
+;    
+;    (dynamic-wind
+;        (lambda () (void))
+;        (lambda ()
+;          (let ([qr (new-default-qr "")])
+;            (draw qr canvas_png_file 'png)
+;            (draw qr canvas_svg_file 'svg)
+;            ))
+;        (lambda ()
+;          (void)
+;          ;(delete-file canvas_png_file)
+;          ;(delete-file canvas_svg_file)
+;          )))
+;
+;   (test-case
+;    "test-fill-canvas"
+;    
+;    (dynamic-wind
+;        (lambda () (void))
+;        (lambda ()
+;          (let ([qr (new-default-qr "chenxiao")])
+;            (set-QR-module_width! qr 20)
+;
+;            (draw qr fill_canvas_png_file 'png)
+;            (draw qr fill_canvas_svg_file 'svg)
+;            ))
+;        (lambda ()
+;          ;(void)
+;          (delete-file fill_canvas_png_file)
+;          (delete-file fill_canvas_svg_file)
+;          )))
+;
+;   (test-case
+;    "test-pattern-canvas"
+;    
+;    (dynamic-wind
+;        (lambda () (void))
+;        (lambda ()
+;          (let ([qr (new-default-qr "chenxiao")])
+;            (set-QR-module_width! qr 20)
+;
+;            (draw qr pattern_canvas_png_file 'png)
+;            (draw qr pattern_canvas_svg_file 'svg)
+;            ))
+;        (lambda ()
+;          ;(void)
+;          (delete-file pattern_canvas_png_file)
+;          (delete-file pattern_canvas_svg_file)
+;          )))
+;   
+;   (test-case
+;    "test-transparent-canvas"
+;
+;    (dynamic-wind
+;        (lambda () (void))
+;        (lambda ()
+;          (let ([qr (new-default-qr "chenxiao")])
+;            (set-QR-module_width! qr 20)
+;
+;            (draw qr transparent_canvas_png_file 'png)
+;            (draw qr transparent_canvas_svg_file 'svg)
+;            ))
+;        (lambda ()
+;          ;(void)
+;          (delete-file transparent_canvas_png_file)
+;          (delete-file transparent_canvas_svg_file)
+;          )))
+;
+;   (test-case
+;    "test-fill-points"
+;    
+;    (dynamic-wind
+;        (lambda () (void))
+;        (lambda ()
+;          (let ([qr (new-default-qr "chenxiao")])
+;            (set-QR-module_width! qr 20)
+;
+;            (draw qr normal_canvas_png_file 'png)
+;            (draw qr normal_canvas_svg_file 'svg)
+;            ))
+;        (lambda ()
+;          ;(void)
+;          (delete-file normal_canvas_png_file)
+;          (delete-file normal_canvas_svg_file)
+;          )))
    ))
 
 (run-tests test-func)
